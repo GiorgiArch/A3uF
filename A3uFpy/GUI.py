18f023009c866aa1bd6319750e0c35c9bd7e8eb5
Giorgia <fiorentino.giorgia@gmail.com>
2022-03-27 10:27:25 -0700

    Removed cached components

--- A3uFpy/GUI.py
+++ A3uFpy/GUI.py
@@ -6,8 +6,11 @@ class Main_window():
         self.main_window = tk.Tk()
         self.main_window.title("A3μF")
         self.main_window.geometry('600x600')
-        self.lbl = tk.Label(self.main_window, text="Welcome to A3μF", bg='red', fg='white', font=("Arial Bold", 50))
-        self.lbl.grid(column=0, row=0)
+        self.title = tk.Label(self.main_window, text="Welcome to A3μF", fg='black', font=("Arial Bold", 50))
+        self.title.grid(column=0, row=0)
+        self.subtitle = tk.Label(self.main_window, text="Automatic Analysis of Archaeological Micro-Fauna",\
+            fg='black', font=("Arial", 20))
+        self.subtitle.grid(column=0, row=1)
 
     def run(self):
         self.main_window.mainloop()
